06f48aad8e26c3846e7557715ae525cb6941b0fc
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2022-03-18 13:57:06 +0100

    remove http request from app component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, throwError} from "rxjs";
-import {catchError, map} from "rxjs/operators";
-import {environment} from "../../environments/environment";
-import {Book} from "../store/models/book.model";
+
 
 @Component({
   selector: 'app-home',
@@ -11,37 +8,7 @@ import {Book} from "../store/models/book.model";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  baseUrl: string = environment.baseUrl;
-
   constructor(private http: HttpClient) { }
-
-  loadItemPerBusinessId() {
-    const result = this.http.get<Book>(`${this.baseUrl}`);
-    console.log(result)
-    return result;
-    // return this.http.get<Book>(`${this.baseUrl}/items/${templateName}/${id}`);
-  }
-
-  // getBook():Observable<any> {
-  //   console.log("called")
-  //
-  //   return this.http.get(url)
-  //     .pipe(map((data: any) => {
-  //         //handle api 200 response code here or you wanted to manipulate to response
-  //         console.log(data)
-  //         return data;
-  //
-  //       }),
-  //       catchError((error) => {    // handle error
-  //
-  //         if (error.status == 404) {
-  //           //Handle Response code here
-  //         }
-  //         return throwError(error);
-  //       })
-  //     );
-  //
-  // }
 
   ngOnInit(): void {
   }
